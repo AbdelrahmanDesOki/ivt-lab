12a968bd1d7464affc9dbeeaf211967a5426802f
AbdelrahmanDesOki <desoki.abdelrahman@gmail.com>
2023-04-18 11:46:30 +0200

    Solve first

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -17,6 +17,8 @@ public class GT4500 implements SpaceShip {
 
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
+  
+
     return false;
   }
 
@@ -79,8 +81,9 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+         firingSuccess = primaryTorpedoStore.fire(1) &&  secondaryTorpedoStore.fire(1);
+         return true;
 
-        break;
     }
 
     return firingSuccess;
